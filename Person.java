eaca7b9f9fd67fa4834ebbf430e4e51d64bea322
Satyam <150027381+SatyamIsSatyam@users.noreply.github.com>
2024-11-11 18:38:34 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,9 @@
+// 1. Write a program to accept name and age of a person from the command prompt (passed as arguments when you execute the class)
+// and ensure that the age entered is >=18 and < 60.
+// Display proper error messages. The program must exit gracefully after displaying the error message in case the arguments passed are not proper.
+// (Hint : Create a user defined exception class for handling errors.)
+
+
 import java.util.Scanner;
 
 class InvalidAgeException extends Exception{
